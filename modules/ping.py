765ed8552bf62aa75e0ca0ae47523d9701c92115
TeesArtem <124548802+TeesArtem@users.noreply.github.com>
2023-12-06 18:56:44 +0500

    Update ping.py

--- modules/ping.py
+++ modules/ping.py
@@ -4,15 +4,18 @@ from discord.ext import commands
 class ping(commands.Cog):
 
   def __init__(self, bot):
-    self.bot = bot
+      self.bot = bot
 
-  @commands.slash_command(name="ping")
+  @commands.slash_command(
+    name="ping",
+    description="Показывает пинг бота"
+  )
   async def ping_command(
     self,
     ctx: discord.ApplicationContext
   ):
     await ctx.respond(
-      f"Pong! {round(self.bot.latency * 1000)}ms"
+        f"Понг! {round(self.bot.latency * 1000)}ms"
     )
 
 def setup(bot):
